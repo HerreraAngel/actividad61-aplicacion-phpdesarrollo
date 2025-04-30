d8178360d1e5a26412c40d713e993cae5d809ec7
HerreraAngel <angelherreraallende@gmail.com>
2025-04-30 11:14:29 +0200

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,5 +1,8 @@
+DROP TABLE IF EXISTS focas;
+
 CREATE TABLE focas (
     foca_id INT AUTO_INCREMENT PRIMARY KEY,
+    codigo_animal VARCHAR(15) NOT NULL UNIQUE,
     nombre VARCHAR(50) NOT NULL,
     edad INT NOT NULL,
     peso DECIMAL(5,2) NOT NULL,
@@ -8,15 +11,11 @@ CREATE TABLE focas (
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO focas (nombre, edad, peso, especie, habitat) VALUES
-('Luna', 5, 120.50, 'Foca monje', 'Océano Atlántico'),
-('Rocky', 8, 150.75, 'Foca gris', 'Mar del Norte'),
-('Marina', 3, 95.20, 'Foca común', 'Pacífico Norte'),
-('Bruno', 6, 130.40, 'Foca leopardo', 'Antártida'),
-('Nina', 4, 110.80, 'Foca monje', 'Mediterráneo'),
-('Oscar', 7, 140.30, 'Foca barbuda', 'Ártico'),
-('Sally', 2, 85.60, 'Foca anillada', 'Mar Báltico'),
-('Max', 9, 160.45, 'Foca gris', 'Atlántico Norte'),
-('Bella', 5, 125.90, 'Foca común', 'Costa de California'),
-('Tommy', 6, 135.00, 'Foca leopardo', 'Océano Antártico');
 
+INSERT INTO focas (codigo_animal, nombre, edad, peso, especie, habitat) VALUES
+('68373285N', 'Nina', 4, 110.80, 'Foca monje', 'Mediterráneo'),
+('15646576K', 'Oscar', 7, 140.30, 'Foca barbuda', 'Ártico'),
+('59635730L', 'Sally', 2, 85.60, 'Foca anillada', 'Mar Báltico'),
+('39703586C', 'Max', 9, 160.45, 'Foca gris', 'Atlántico Norte'),
+('08902068X', 'Bella', 5, 125.90, 'Foca común', 'Costa de California'),
+('89957452J', 'Tommy', 6, 135.00, 'Foca leopardo', 'Océano Antártico');
